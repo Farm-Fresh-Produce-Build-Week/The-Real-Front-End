b45820846472fbfe3d3646eae29d61f133bac46c
Laura <theimer.laura@gmail.com>
2019-12-21 11:51:10 -0500

    fixed names of icons due to errors of the naming. Set up dashboardfarmer.js.  Added a note on line 36 about how to set up items for sale

--- market/src/components/DashboardFarmer.js
+++ market/src/components/DashboardFarmer.js
@@ -1,10 +1,45 @@
 import React from "react";
 
-const  DashboardFarmer = () => {
+import farmer3 from "../../Images/Farmer/farmer3.jpg";
+import starfull from "../icons/PNG/starfull.png";
+import starhalf from "../icons/PNG/starhalf.png"; 
+
+const  DashboardFarmer = (props) => {
   return (
     <>
-      <div className="FarmerLandingPage">FarmerLandingPage</div>
-    </>
+      <div className="FarmerLandingPage">
+        <div className="Top-Section">
+          <div className="Farmer-Details">
+            <img src={farmer3} alt={mockfarmer} />
+            <h1> Farmer Joe </h1>  
+          </div> 
+          <div className="ratings-area">
+            <h3> Your Farm Rating: </h3>
+              <div className="stars-rating">
+                <img src={starfull} alt="star-full" />
+                <img src={starfull} alt="star-full" />
+                <img src={starfull} alt="star-full" />
+                <img src={starhalf} alt="star-half" />
+                <img src={starempty} alt="star-empty" />
+              </div> 
+          </div> 
+          <div className="button-area">
+            <button> Add Items to Sell </button>
+                {/* sends to another component not named yet..  */}
+            <button> Farm Details </button>
+                {/* dead button may add page for fun later  */}
+          </div> 
+        </div> 
+
+        <div className="Sale-Section">
+          <div className="Items-For-Sale" />
+          {/* Throw is in A) <itemlistcomponent /> that lists over each item for sale  B) a <itemcomponent /> of mock data?   */}
+        </div>
+            {/* end of sale-section */}
+        
+      </div> 
+          {/* end of DashboardFarmer */}
+      </>
   );
 };
 
