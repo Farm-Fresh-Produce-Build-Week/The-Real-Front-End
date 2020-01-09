36f717a66ff5ab7c1e2dfd49b37624f5cb8ce0ee
Laura <theimer.laura@gmail.com>
2020-01-09 00:11:15 -0500

    initial set up of Cart.js

---
+++ market/src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+const Cart = () => {
+    const { cart } = useContext(CartContext); 
+    return (
+        <>
+        <div className="Cart-Page">
+
+        </div>
+        </>
+    )
+}
